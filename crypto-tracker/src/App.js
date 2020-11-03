988a2eaf6620722f6891ef396c7e58f7ccc34fb3
wesley.moody <wgm3190@gmail.com>
2020-11-03 16:04:35 -0600

    app.js setup added coin.js

---
+++ crypto-tracker/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => alert("You fucked up"));
+  }, []);
+
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a Currency</h1>
+        <form>
+          <input type='text' placeholder='Search' className='coin-input' />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
